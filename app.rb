cc261a1a8f7d133e837946e7143dd8cb6459eeae
Jessie <lonsdale.jess@gmail.com>
2020-06-16 11:35:32 +0100

    serving static files

--- app.rb
+++ app.rb
@@ -2,13 +2,17 @@ require "sinatra"
 require "sinatra/json"
 require_relative('./wav_reader.rb')
 
+set :sound_folder, 'audio_files'
+
+
 get '/' do
-  # json :test => 'jess'
-    "Hello World!"
+  'hello this is our sound api :D'
 end
 
-
 get '/rain' do
-  test = WavReader.new('test')
-  return test.read
+  send_file File.join(settings.sound_folder, 'rain_1.wav')
+end
+
+get '/chickens' do
+  send_file File.join(settings.sound_folder, 'chickens_1.wav')
 end
